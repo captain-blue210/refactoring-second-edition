17aa28531b2bd7898d059bae1691787c001d4a7a
captain-blue210 <captain.blue210@gmail.com>
2021-04-03 09:39:14 +0900

    refactor : change argument name

--- src/chapter1/index.js
+++ src/chapter1/index.js
@@ -23,14 +23,14 @@ export function statement(invoice, plays) {
 
   return result;
 
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
     // 喜劇のときは 10人につき、 さらにポイントを加算
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
 
-    return volumeCredits;
+    return result;
   }
 
   function playFor(aPerformance) {
