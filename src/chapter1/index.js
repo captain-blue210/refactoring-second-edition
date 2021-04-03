f16f92fc226c0a5ab4744bba16ac24d21cf8e2b5
captain-blue210 <captain.blue210@gmail.com>
2021-04-03 09:37:21 +0900

    refactor : extract calculate volumeCreditsFor

--- src/chapter1/index.js
+++ src/chapter1/index.js
@@ -10,13 +10,9 @@ export function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // ボリューム特典のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 喜劇のときは 10人につき、 さらにポイントを加算
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-    // 注文の内訳を出力
+    volumeCredits += volumeCreditsFor(perf);
 
+    // 注文の内訳を出力
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
@@ -26,6 +22,16 @@ export function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
 
   return result;
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 喜劇のときは 10人につき、 さらにポイントを加算
+    if ('comedy' === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    return volumeCredits;
+  }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
