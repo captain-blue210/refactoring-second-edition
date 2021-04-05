140601a2864505be8bea07d147af6af102788989
captain-blue210 <captain.blue210@gmail.com>
2021-04-05 23:13:44 +0900

    refactor : move volumeCreditsFor method and replace variable

--- src/chapter1/index.js
+++ src/chapter1/index.js
@@ -8,6 +8,7 @@ export function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -35,6 +36,16 @@ export function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇のときは 10人につき、 さらにポイントを加算
+    if ('comedy' === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+
     return result;
   }
 }
@@ -65,7 +76,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += perf.volumeCredits;
     }
     return volumeCredits;
   }
@@ -76,15 +87,5 @@ function renderPlainText(data, plays) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは 10人につき、 さらにポイントを加算
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
   }
 }
