46b23331b10639cab4e83a1451a5f0c37f04c344
captain-blue210 <captain.blue210@gmail.com>
2021-04-10 17:38:09 +0900

    refactor : add ComedyCalculator class and fix PerformanceCalculator amount method.

--- src/chapter1/createStatementData.js
+++ src/chapter1/createStatementData.js
@@ -38,22 +38,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case 'tragedy':
-        throw '想定外の呼び出し';
-        break;
-      case 'comedy':
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`未知の演劇の種類: ${this.play.type}`);
-    }
-    return result;
+    throw new Error(`サブクラスの責務`);
   }
 
   get volumeCredits() {
@@ -88,4 +73,12 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    return (result += 300 * this.performance.audience);
+  }
+}
